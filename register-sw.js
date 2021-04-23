f95fa0f926fc1c666bd598518da113aefe9915a6
Araraura <yarinpit@gmail.com>
2021-04-23 11:07:49 +0300

    Service worker

---
+++ register-sw.js
@@ -0,0 +1,16 @@
+if ('serviceWorker' in navigator) {
+    window.addEventListener('load', () => {
+        navigator.serviceWorker.register('/service-worker.js');
+    });
+}
+self.addEventListener('install', (event) => {
+    console.log('Inside the install handler:', event);
+});
+
+self.addEventListener('activate', (event) => {
+    console.log('Inside the activate handler:', event);
+});
+
+self.addEventListener(fetch, (event) => {
+    console.log('Inside the fetch handler:', event);
+});
