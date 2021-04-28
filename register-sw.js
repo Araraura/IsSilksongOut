d9ed0198e8d437e7123dee4c0ce26a8b5c1b1378
Araraura <yarinpit@gmail.com>
2021-04-28 20:38:39 +0300

    trying an updated sw register

--- register-sw.js
+++ register-sw.js
@@ -1,16 +1,10 @@
+// Register the service worker
 if ('serviceWorker' in navigator) {
-    window.addEventListener('load', () => {
-        navigator.serviceWorker.register('/service-worker.js');
+	navigator.serviceWorker.register('/service-worker.js').then(function(registration) {
+    // Registration was successful
+    console.log('ServiceWorker registration successful with scope: ', registration.scope);
+}).catch(function(err) {
+    // registration failed
+    	console.log('ServiceWorker registration failed: ', err);
     });
 }
-self.addEventListener('install', (event) => {
-    console.log('Inside the UPDATED install handler:', event);
-});
-
-self.addEventListener('activate', (event) => {
-    console.log('Inside the activate handler:', event);
-});
-
-self.addEventListener(fetch, (event) => {
-    console.log('Inside the fetch handler:', event);
-});
