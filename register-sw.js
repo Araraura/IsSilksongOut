4a2279f4fd8cf454f4e3097509e0025b711d16f3
Araraura <yarinpit@gmail.com>
2021-12-03 20:03:07 +0200

    Replaced spaces with tabs wherever possible

--- register-sw.js
+++ register-sw.js
@@ -1,10 +1,5 @@
 // Register the service worker
 if ('serviceWorker' in navigator) {
-	navigator.serviceWorker.register('/service-worker.js').then(function(registration) {
-    // Registration was successful
-    console.log('ServiceWorker registration successful with scope: ', registration.scope);
-}).catch(function(err) {
-    // registration failed
-    	console.log('ServiceWorker registration failed: ', err);
-    });
+	navigator.serviceWorker
+		.register('/service-worker.js');
 }
